c23d8bffad9d3c4b230a8fbc1885122e8aeafc78
7csn <1479672753@qq.com>
2021-06-03 14:28:08 +0800

    .

--- src/traits/ServerContext.php
+++ src/traits/ServerContext.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace chaser\stream\traits;
 
+use chaser\stream\interfaces\part\ContextInterface;
 use chaser\stream\Port;
 
 /**
  * 服务器的流资源上下文特征
  *
  * @package chaser\stream\traits
+ *
+ * @see ContextInterface
  */
 trait ServerContext
 {
